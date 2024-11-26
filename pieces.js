7044f306c370a7d29e4967941a8621734c0c6069
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-11-26 08:26:07 +0100

    Added newly created HTML elements to the DOM.

--- pieces.js
+++ pieces.js
@@ -11,7 +11,13 @@ const nomElement = document.createElement("h2");
 nomElement.innerText = article.nomElement;
 
 const prixElement = document.createElement("p");
-prixElement.innerText = `Prix : ${article.prix} &#8364;`;
+prixElement.innerText = `Prix : ${article.prix} €`;
 
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie;
+
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiche.appendChild(categorieElement);
