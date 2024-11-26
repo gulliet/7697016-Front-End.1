be26e5ca3b67d2768c18ec7c92ef139b491194c5
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-11-26 08:18:06 +0100

    Created HTML elements

--- pieces.js
+++ pieces.js
@@ -1,3 +1,17 @@
 // Récupération des données dans le fichier JSON
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
+
+const article = pieces[0];
+
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nomElement;
+
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix : ${article.prix} &#8364;`;
+
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie;
