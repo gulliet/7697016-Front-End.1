c6dcebe4147f866e8024165e41878f3635ab6749
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-11-26 08:47:13 +0100

    Display product avaibility.

--- pieces.js
+++ pieces.js
@@ -22,9 +22,15 @@ const descriptionElement = document.createElement("p");
 descriptionElement.innerText =
     article.description ?? "Pas de description pour le moment";
 
+const disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = `${
+    article.disponibilite ? "En stock" : "Rupture de stock"
+}`;
+
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
 sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(disponibiliteElement);
