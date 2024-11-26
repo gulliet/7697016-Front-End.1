0749b0a89c6ef3b324c85efda680e4f7902e26fd
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-11-26 10:37:42 +0100

    Added sort function.

--- pieces.js
+++ pieces.js
@@ -1,28 +1,32 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('pieces-autos.json');
+const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
 for (let i = 0; i < pieces.length; i++) {
-
     const article = pieces[i];
     // Récupération de l'élément du DOM qui accueillera les fiches
     const sectionFiches = document.querySelector(".fiches");
     // Création d’une balise dédiée à une pièce automobile
     const pieceElement = document.createElement("article");
-    // Création des balises 
+    // Création des balises
     const imageElement = document.createElement("img");
     imageElement.src = article.image;
     const nomElement = document.createElement("h2");
     nomElement.innerText = article.nom;
     const prixElement = document.createElement("p");
-    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    prixElement.innerText = `Prix: ${article.prix} € (${
+        article.prix < 35 ? "€" : "€€€"
+    })`;
     const categorieElement = document.createElement("p");
     categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
     const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    descriptionElement.innerText =
+        article.description ?? "Pas de description pour le moment.";
     const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-    
+    stockElement.innerText = article.disponibilite
+        ? "En stock"
+        : "Rupture de stock";
+
     // On rattache la balise article a la section Fiches
     sectionFiches.appendChild(pieceElement);
     // On rattache l’image à pieceElement (la balise article)
@@ -33,6 +37,12 @@ for (let i = 0; i < pieces.length; i++) {
     //Ajout des éléments au DOM pour l'exercice
     pieceElement.appendChild(descriptionElement);
     pieceElement.appendChild(stockElement);
+}
 
- }
- 
+const btnTrier = document.querySelector(".btn-trier");
+btnTrier.addEventListener("click", () => {
+    pieces.sort((a, b) => {
+        return a.prix - b.prix;
+    });
+    console.log(pieces);
+});
